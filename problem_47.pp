c488f88c0f999da8da92c0531fad30d553490994
bvbesouw <bvbesouw@gmail.com>
2022-06-10 21:30:55 -0400

    Update problem_47.pp

--- problem_47.pp
+++ problem_47.pp
@@ -1,27 +1,61 @@
-
-PROGRAM problem_47;
-
+program problem_47;
 {$mode ObjFPC}{$H+}
-
-USES 
-fgl,
-classes,
-sysutils;
-
-CONST 
-  mx = 1000000;
-
-  //TYPE
-  //TIntegerList = Specialize TfpgList<longint>;
+uses fgl,sysutils;
+TYPE
+  TIntegerList = Specialize TfpgList<integer>;
 
 VAR 
-  list: Tstringlist;
+  list: Tintegerlist;
+
+function BinarySearch(X: integer; var indx: integer): boolean;
+var
+top, bottom, middle,toosmall,i :integer; 
+found : boolean;
+begin
+  top := pred(list.count);
+  bottom := 0;
+  found := false;
+  indx := -1;
+  repeat
+    middle := (top + bottom) div 2;
+    if (list[top] = x) or (list[middle]= x) or (list[bottom]=x) then
+    begin
+      found := true;
+      if list[top] = x then indx := top else
+      if list[middle] = x then indx := middle else
+      indx:= bottom;
+    end;
+    toosmall := top - bottom;
+    if x > list[middle] then bottom := middle else top:= middle;
+  until (found or (toosmall < 3));
+  result := found;
+  if not found then
+  begin
+    indx := top;
+    while list[indx] > x do dec(indx);
+    inc(indx);
+  end;
+end;
+
+procedure AddToList(x : integer);
+var ix : integer;
+begin
+  if list.count = 0 then list.add(x) else 	
+    if x > list[pred(list.count)] then list.add(x) else
+      if x < list[0] then list.insert(0,x) else
+        if not BinarySearch(x,ix) then list.insert(ix,x);
+end;
+
+
+
 
 FUNCTION npf(number: Integer): Integer;
 
 VAR 
   i: Integer;
+
 BEGIN
+
   i := 2;
   list.clear;
   WHILE (i < sqrt(number)) OR (number = 1) DO
@@ -29,7 +63,7 @@ BEGIN
       IF number MOD i = 0 THEN
         BEGIN
           number := number DIV i;
-          list.add(inttostr(i));
+          AddToList(i);
           dec(i);
         END;
       inc(i);
@@ -41,9 +75,8 @@ VAR
   j: Integer;
 
 BEGIN
-  list := Tstringlist.Create;
-  list.sorted := true;
-  list.duplicates := dupIgnore;
+  list := Tintegerlist.Create;
+
   j := 2 * 3 * 5 * 7;
   WHILE True DO
     BEGIN
@@ -55,5 +88,6 @@ BEGIN
         END;
       inc(j);
     END;
+
   list.Free;
 END.
